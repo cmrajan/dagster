bd07f147a56c3ad3cf7752be5e2a4f77369faf33
Sashank Thupukari <s@thupukari.com>
2020-03-17 11:09:32 -0400

    Show new schedule ticks in Dagit

--- js_modules/dagit/src/schedules/ScheduleRoot.tsx
+++ js_modules/dagit/src/schedules/ScheduleRoot.tsx
@@ -22,6 +22,17 @@ import { showCustomAlert } from "../CustomAlertProvider";
 import { unixTimestampToString } from "../Util";
 import { RunStatus } from "../runs/RunUtils";
 import styled from "styled-components/macro";
+import {
+  Collapse,
+  Divider,
+  Icon,
+  Intent,
+  Callout,
+  Code
+} from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+
+import { useState } from "react";
 
 const NUM_RUNS_TO_DISPLAY = 10;
 const NUM_ATTEMPTS_TO_DISPLAY = 25;
@@ -75,69 +86,101 @@ interface AttemptsTableProps {
 const AttemptsTable: React.FunctionComponent<AttemptsTableProps> = ({
   attemptList
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   if (!attemptList || !attemptList.length) {
     return null;
   }
   return (
-    <>
-      <Header>Attempts</Header>
-      {attemptList.map((attempt, i) => (
-        <RowContainer key={i} style={{ marginBottom: 0, boxShadow: "none" }}>
-          <RowColumn
-            style={{
-              maxWidth: 30,
-              borderRight: 0,
-              padding: 7
-            }}
-          >
-            {attempt.run ? (
-              <RunStatus status={attempt.run.status} />
-            ) : (
-              <AttemptStatus status={attempt.status} />
-            )}
-          </RowColumn>
-          <RowColumn style={{ textAlign: "left", borderRight: 0 }}>
-            {attempt.run ? (
-              <div>
-                <Link to={`/runs/all/${attempt.run.runId}`}>
-                  {attempt.run.runId}
-                </Link>
-              </div>
-            ) : (
-              <div>
-                <ButtonLink
-                  onClick={() =>
-                    showCustomAlert({
-                      title: "Schedule Response",
-                      body: (
-                        <>
-                          <HighlightedCodeBlock
-                            value={JSON.stringify(
-                              JSON.parse(attempt.jsonResult),
-                              null,
-                              2
-                            )}
-                            languages={["json"]}
-                          />
-                        </>
-                      )
-                    })
-                  }
-                >
-                  {" "}
-                  View error
-                </ButtonLink>
-              </div>
-            )}
-          </RowColumn>
-          <RowColumn
-            style={{ maxWidth: 200, paddingLeft: 0, textAlign: "left" }}
-          >
-            {unixTimestampToString(attempt.time)}
-          </RowColumn>
-        </RowContainer>
-      ))}
-    </>
+    <AttemptsTableContainer>
+      <Header>
+        Old Attempts (Deprecated){" "}
+        <Icon
+          style={{ cursor: "pointer" }}
+          icon={isOpen ? IconNames.CHEVRON_DOWN : IconNames.CHEVRON_RIGHT}
+          iconSize={Icon.SIZE_LARGE}
+          intent={Intent.PRIMARY}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </Header>
+      <Divider />
+      <Collapse isOpen={isOpen}>
+        <Callout
+          title={
+            "These schedule attempts will be no longer visible in Dagit 0.8.0"
+          }
+          intent={Intent.WARNING}
+          style={{ margin: "10px 0" }}
+        >
+          <p>
+            The way Dagster stores schedule attempts has been updated to use a
+            database, which can be configured using{" "}
+            <a href="https://docs.dagster.io/latest/deploying/instance/#scheduler-storage">
+              <Code>Scheduler Storage</Code>
+            </a>
+            . Previously, attempts were stored on the fileystem at{" "}
+            <Code>$DAGSTER_HOME/schedules/logs/</Code>. This update removes the
+            dependency on the filesytem for the scheduler, and enables the
+            durability of schedule attempt history across deploys of dagster.
+          </p>
+        </Callout>
+        {attemptList.map((attempt, i) => (
+          <RowContainer key={i} style={{ marginBottom: 0, boxShadow: "none" }}>
+            <RowColumn
+              style={{
+                maxWidth: 30,
+                borderRight: 0,
+                padding: 7
+              }}
+            >
+              {attempt.run ? (
+                <RunStatus status={attempt.run.status} />
+              ) : (
+                <AttemptStatus status={attempt.status} />
+              )}
+            </RowColumn>
+            <RowColumn style={{ textAlign: "left", borderRight: 0 }}>
+              {attempt.run ? (
+                <div>
+                  <Link to={`/runs/all/${attempt.run.runId}`}>
+                    {attempt.run.runId}
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <ButtonLink
+                    onClick={() =>
+                      showCustomAlert({
+                        title: "Schedule Response",
+                        body: (
+                          <>
+                            <HighlightedCodeBlock
+                              value={JSON.stringify(
+                                JSON.parse(attempt.jsonResult),
+                                null,
+                                2
+                              )}
+                              languages={["json"]}
+                            />
+                          </>
+                        )
+                      })
+                    }
+                  >
+                    {" "}
+                    View error
+                  </ButtonLink>
+                </div>
+              )}
+            </RowColumn>
+            <RowColumn
+              style={{ maxWidth: 200, paddingLeft: 0, textAlign: "left" }}
+            >
+              {unixTimestampToString(attempt.time)}
+            </RowColumn>
+          </RowContainer>
+        ))}
+      </Collapse>
+    </AttemptsTableContainer>
   );
 };
 
@@ -176,6 +219,9 @@ export const SCHEDULE_ROOT_QUERY = gql`
   ${ScheduleRowFragment}
 `;
 
+const AttemptsTableContainer = styled.div`
+  margin: 20px 0;
+`;
 const ButtonLink = styled.button`
   color: #106ba3;
   margin-left: 10px;
